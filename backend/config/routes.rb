52cbcc99c38709ca168d51a19ce60fdfa72f9fd8
Fabio Daguer Esposito <fabio.daguer@gmail.com>
2021-10-07 06:43:46 -0300

    chore: move backend app to ./backend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   defaults format: :json do
+    root 'carts#show'
+
     resources :carts, only: :show do
       resources :cart_items, as: :items, except: %i[edit index]
     end
